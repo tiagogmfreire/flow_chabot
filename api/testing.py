4581d605823e7428fbfec3ba1539e74682075ee0
tiagogmfreire <tiagogmfreire@gmail.com>
2025-10-07 15:27:39 -0300

    gerando token do flow

--- api/testing.py
+++ api/testing.py
@@ -1,5 +1,7 @@
 import os
+import json
 import requests
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,9 +10,25 @@ base_url = os.getenv("BASE_URL")
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
-response = requests.get(
-    str(base_url + "/ai-orchestration-api/v1/health")
-)
+headers = {
+    'accept'       : '/',
+    'Content-Type' : 'application/json',
+    'FlowTenant'   : 'Stretto',
+}
 
-print(response.status_code)
-print(response.text)
+payload = {
+    'clientId'      : client_id,
+    'clientSecret'  : client_secret,
+    'appToAccess'   : "llm-api"
+}
+
+url = str(base_url + "/auth-engine-api/v1/api-key/token")
+
+response = requests.post(url, headers=headers, data=json.dumps(payload))
+
+json = response.json()
+
+print(json['access_token'])
+
+# print(response.status_code)
+# print(response.json()))
