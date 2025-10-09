e683c5def68adc97072f86d726834da10d4f828e
tiagogmfreire <tiagogmfreire@gmail.com>
2025-10-09 18:00:22 -0300

    installing sentence transformers and sending document chunks to Chroma

--- api/rag.py
+++ api/rag.py
@@ -1,13 +1,13 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chat_models import init_chat_model
-from langchain_openai import OpenAIEmbeddings
-from langchain_openai import AzureChatOpenAI
-from langchain_openai import AzureOpenAIEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
+from sentence_transformers import SentenceTransformer
 from langchain_chroma import Chroma
 from flow import generate_token
 from dotenv import load_dotenv
 import os
+import sys
 
 load_dotenv()
 
@@ -33,52 +33,21 @@ text_splitter = RecursiveCharacterTextSplitter(
     chunk_overlap=200,  # chunk overlap (characters)
     add_start_index=True,  # track index in original document
 )
+
 all_splits = text_splitter.split_documents(pages)
 
 # print(f"Split file {len(all_splits)} sub-documents.")
 
-flow_token = generate_token()
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
 
-# print(flow_token)
-
-# chat model
-# llm = init_chat_model(
-#     "gpt-4o-mini", 
-#     model_provider="azure-openai",
-#     azure_endpoint=os.getenv('BASE_URL'),
-#     openai_api_key=flow_token,
-#     api_version='3.0.0'
-# )
-
-llm = AzureChatOpenAI(
-    openai_api_key=flow_token,
-    azure_endpoint=os.getenv('BASE_URL'),
-    azure_deployment='flow-core-llm-eaus-ca',
-    openai_api_version='3.0.0',
-)
-
-# print(llm)
-
-# embeddings model
-embeddings = OpenAIEmbeddings(
-    model="text-embedding-ada-002",
-    openai_api_base=os.getenv('BASE_URL'),
-    openai_api_key=flow_token
-)
-
-# print(embeddings)
-
-# vector store
 vector_store = Chroma(
     collection_name="example_collection",
     embedding_function=embeddings,
     persist_directory="./chroma_langchain_db",  # Where to save data locally, remove if not necessary
 )
 
-# print(vector_store)
-
 document_ids = vector_store.add_documents(documents=all_splits)
 
-# print(document_ids)
+print(document_ids)
 
-# print(document_ids[:3])
+print(document_ids[:3])
