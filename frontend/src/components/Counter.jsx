b0c8d17faa3444340afd57a4df19b55933dce3e8
tiagogmfreire <tiagogmfreire@gmail.com>
2025-10-15 14:46:56 -0300

    Validating react integration with a basic component

---
+++ frontend/src/components/Counter.jsx
@@ -0,0 +1,13 @@
+// src/components/Counter.jsx
+import React, { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Current count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+}
